16741af6a2f691ca89a98958bf6ec9fef5abb70d
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-12-12 22:02:56 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email) {
+  
+    return fetch('http://localhost:3000/users',{
+      method: 'POST',
+      headers:{
+        'Content-type':'application/json',
+        Accept:'application/json'
+      },
+      body: JSON.stringify({
+        name:name,
+        email:email
+      })
+    })
+    .then(res => res.json())
+    .then(Object => {document.body.innerHTML = object["id"]})
+    .catch(error =>{document.body.innerHTML = error.message})  
+    
+}
